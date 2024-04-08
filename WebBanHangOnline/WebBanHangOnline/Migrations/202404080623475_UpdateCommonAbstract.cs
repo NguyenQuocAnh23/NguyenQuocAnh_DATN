56f876486f65ea063d3bccb19cf800dbb4a3b1aa
NguyenQuocAnh23 <enkiuay23@gmail.com>
2024-04-08 15:16:18 +0700

    update

---
+++ WebBanHangOnline/WebBanHangOnline/Migrations/202404080623475_UpdateCommonAbstract.cs
@@ -0,0 +1,24 @@
+﻿namespace WebBanHangOnline.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class UpdateCommonAbstract : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.tb_Posts", "CreatedBy", c => c.String());
+            AddColumn("dbo.tb_Posts", "CreatedDate", c => c.DateTime(nullable: false));
+            AddColumn("dbo.tb_Posts", "ModifierDate", c => c.DateTime(nullable: false));
+            AddColumn("dbo.tb_Posts", "ModifierBy", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.tb_Posts", "ModifierBy");
+            DropColumn("dbo.tb_Posts", "ModifierDate");
+            DropColumn("dbo.tb_Posts", "CreatedDate");
+            DropColumn("dbo.tb_Posts", "CreatedBy");
+        }
+    }
+}
