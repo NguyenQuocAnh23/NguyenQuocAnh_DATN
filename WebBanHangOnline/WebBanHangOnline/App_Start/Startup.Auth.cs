83d8d2e5ed817e5b67cdfb726c73ca5a966af4ae
NguyenQuocAnh23 <enkiuay23@gmail.com>
2024-05-10 03:20:45 +0700

    update

--- WebBanHangOnline/WebBanHangOnline/App_Start/Startup.Auth.cs
+++ WebBanHangOnline/WebBanHangOnline/App_Start/Startup.Auth.cs
@@ -60,8 +60,8 @@ namespace WebBanHangOnline
 
             //app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
             //{
-            //    ClientId = "683692821698-3jndj924hd6vsog3he00s2bksfpdp8l4.apps.googleusercontent.com",
-            //    ClientSecret = "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"
+            //    ClientId = "",
+            //    ClientSecret = ""
             //});
         }
     }
