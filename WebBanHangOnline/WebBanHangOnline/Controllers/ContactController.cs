4d387b8e6f739adde25650bedc879626ab76dfe0
NguyenQuocAnh23 <enkiuay23@gmail.com>
2024-05-25 13:32:05 +0700

    OK

--- WebBanHangOnline/WebBanHangOnline/Controllers/ContactController.cs
+++ WebBanHangOnline/WebBanHangOnline/Controllers/ContactController.cs
@@ -1,17 +1,39 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebBanHangOnline.Models;
+using System.Web.Helpers;
 
 namespace WebBanHangOnline.Controllers
 {
     public class ContactController : Controller
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
         // GET: Contact
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult LichSuDonHang()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                var userStore = new UserStore<ApplicationUser>(new ApplicationDbContext());
+                var userManager = new UserManager<ApplicationUser>(userStore);
+                var user = userManager.FindByName(User.Identity.Name);
+
+                string email = user.Email;
+
+                var items = db.Orders.Where(x => x.Email == email).ToList();
+                return PartialView(items);
+            }
+
+            return PartialView();
         }
     }
 }
